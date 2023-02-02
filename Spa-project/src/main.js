2c70b3d49c9e4dbc496e1a6cabed6ebc586b2a74
Tim Viklund Fjesme <tim.viklund.fjesme@iths.se>
2023-02-02 11:18:30 +0100

    2

--- Spa-project/src/main.js
+++ Spa-project/src/main.js
@@ -1,14 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// Import bootstrap and js that bootstrap uses
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/base.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(bootstrap)
 
 app.mount('#app')
